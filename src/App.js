4b5f2d3619ff076908f1d22c9d4df5acd9c1dccd
arunbanga <arunbanga810@gmail.com>
2022-10-30 15:18:58 +0530

    new project add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Footer } from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "../src/Components/Pages/Home";
+import Login from "./Components/Pages/Login/Login";
+import { Globalstyle } from "./Components/Styles/Globalstyle";
+const App = () => {
+  const theme = {
+    color: {
+      bg: "#479DF3 ",
+      heading: "#F6FAFE",
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Globalstyle />
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+            </Routes>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
-}
+};
 
 export default App;
