20550686630647f1e50ec5179cad80e5785cb44e
arunbanga <arunbanga810@gmail.com>
2022-10-30 15:21:37 +0530

    globalstyle page add

---
+++ src/Components/Styles/Globalstyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+export const Globalstyle = createGlobalStyle`
+*{
+    margin:0px;
+    padding:0px;
+    box-sizing:border-box;
+    font-family: 'Source Sans Pro', sans-serif;
+}
+html{
+    font-size:62.5%;
+    overflow-x:hidden;
+}
+body{
+    overflow-x:hidden;
+}
+
+`;
