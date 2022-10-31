1183ce76f509d1de0dbf186b7f46e9416189fc7f
arunbanga <arunbanga810@gmail.com>
2022-10-31 20:03:53 +0530

    Header page logo position change & media queries add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,9 @@ const Header = () => {
         <div className="container">
           <div className="logo">
             <h3>Flipkart</h3>
-            <p>Explore Plus+</p>
+            <p>
+              Explore <span>Plus+</span>
+            </p>
           </div>
           <div className="main-container">
             <Navbar />
@@ -30,21 +32,38 @@ const Wrapper = styled.div`
   color: ${({ theme }) => theme.color.heading};
   h3 {
     font-size: 2.5rem;
-    display: flex;
   }
   p {
     font-size: 1.2rem;
+    span {
+      color: #f9f038;
+      font-weight: 700;
+    }
   }
-  div {
-    padding-left: 2rem;
-  }
+
   .container {
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: space-around;
+    position: relative;
   }
   .logo {
+    position: absolute;
+    left: 110px;
+  }
+  @media (max-width: ${({ theme }) => theme.media.tab}) {
+    .logo {
+      position: absolute;
+      left: 60px;
+    }
+    .container {
+      width: 95%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      position: relative;
+    }
   }
 `;
 export default Header;
